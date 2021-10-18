49fc5b29de9677722856c293952ff3a894a6aa11
Herman <rullherman@gmail.com>
2021-10-18 19:05:38 +0300

    Tasks 1-4 completed

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 
 $.ajax({
-  url: "https://api.jsonbin.io/b/616d685d4a82881d6c61dd8f",
+  url: "https://api.jsonbin.io/b/616d685d4a82881d6c61dd8f/1",
   headers: {
     "secret-key": "$2b$10$g0YD28kxgaYz2SeiBkEnRus8wNSVgngZzDfABrsqe7SuZEPNra/Oa",
   },
   type: "GET",
   success: function (data) {
     console.log(data);
-    $("#post-list").html(
+    $("#post-list").html(function(){
+      var html ="";
       data.posts.map(post => 
-        <div class="post">
-          <div class="top-bar">
-            <img src={post.profile_picture}/>
-            <p1 class ="date">Sep 18, 2020 15: 16</p1>
-          </div>
-          <div class="img-container">
-            <img src={post.img_link}/>
-          </div>
-          <p1>{post.title}</p1>
-          <img src="https://w7.pngwing.com/pngs/440/84/png-transparent-white-and-blue-thumbs-up-logo-facebook-like-button-computer-icons-free-s-like-button-miscellaneous-text-hand.png" />
-        </div>
-      )
-    )
+        html = html.concat(
+        '<div class="post">'+
+          '<div class="top-bar">'+
+            '<img src='+post.profile_picture+'>'+
+            '<p1 class ="date">'+ post.post_time +'</p1>'+
+          '</div>'+ 
+          getImageDivIfExists(post.img_link)+
+          '<p1>'+post.title+'</p1>'+
+          '<img src="https://w7.pngwing.com/pngs/440/84/png-transparent-white-and-blue-thumbs-up-logo-facebook-like-button-computer-icons-free-s-like-button-miscellaneous-text-hand.png">'+
+        '</div>')
+        );
+      console.log(html)
+      return html;
+
+    });
   },
+});
+var getImageDivIfExists = ((img_link) => {
+  
+    if(img_link=="None"){
+      return "";
+    }
+    return(
+    '<div class="img-container">'+
+      '<img src='+img_link+'>'+
+    '</div>'
+    );
 });
