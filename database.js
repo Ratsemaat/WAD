c0f1e43a12d3e86b22d7934a957a762aeda33de0
KaurTaal <taalkaur@gmail.com>
2021-12-09 00:22:26 +0200

    like implementing

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Pool = require('pg').Pool; 
-const pool = new Pool({ 
-    user: "postgres", 
-    password: "root", 
-    database: "WAD", 
-    host: "localhost", 
-    port: "5432" 
+const Pool = require("pg").Pool;
+const pool = new Pool({
+  user: "postgres",
+  password: "Muumimaa65",
+  database: "WAD",
+  host: "localhost",
+  port: "5432",
 });
 module.exports = pool;
